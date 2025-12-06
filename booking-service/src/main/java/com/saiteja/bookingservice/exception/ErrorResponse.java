05c6e99e1427b01497fc453e092ae1c8ad4139c4
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-06 18:55:00 +0530

    added eureka server and exceptions

---
+++ booking-service/src/main/java/com/saiteja/bookingservice/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.saiteja.bookingservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private String status;
+    private LocalDateTime timestamp;
+}
+
