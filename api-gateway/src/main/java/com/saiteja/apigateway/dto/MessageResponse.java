256b3ab3ee5637ccea65d891e53aa9142db60a55
saiteja-in <vurukondasaiteja13@gmail.com>
2025-12-08 01:42:48 +0530

    fix

--- api-gateway/src/main/java/com/saiteja/apigateway/dto/MessageResponse.java
+++ api-gateway/src/main/java/com/saiteja/apigateway/dto/MessageResponse.java
@@ -1,10 +1,18 @@
 package com.saiteja.apigateway.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class MessageResponse {
     private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
+
--- api-gateway/src/main/java/com/saiteja/apigateway/dto/SignUpRequest.java
+++ api-gateway/src/main/java/com/saiteja/apigateway/dto/SignUpRequest.java
@@ -3,11 +3,9 @@ package com.saiteja.apigateway.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
 import java.util.Set;
 
-@Data
 public class SignUpRequest {
     @NotBlank
     @Size(min = 3, max = 20)
@@ -23,4 +21,37 @@ public class SignUpRequest {
     @NotBlank
     @Size(min = 6, max = 40)
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
 }
+
